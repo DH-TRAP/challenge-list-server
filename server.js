c75a3291b2fe3c34ba253e5d56d43c720e8335ea
PARTH DOBARIYA <dobariya1212@gmail.com>
2023-05-23 17:53:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const url = require('url');
 const fs = require('fs');
 require('dotenv').config();
 
-const hostname = process.env.HOST ? '127.0.0.1':null;
-const port = process.env.PORT || 8000;
+// const hostname = process.env.HOST ? '127.0.0.1' : null;
+const port = 10000;
 
-const server = http.createServer((req,res) => {
-  const {query, path} = url.parse(req.url, true);
+const server = http.createServer((req, res) => {
+  const { query, path } = url.parse(req.url, true);
 
   const home = fs.readFileSync(`${__dirname}/home.html`, 'utf-8');
   const challenge1 = fs.readFileSync(`${__dirname}/challenges/1-Top-Double.html`, 'utf-8');
@@ -16,13 +16,13 @@ const server = http.createServer((req,res) => {
   const challenge4 = fs.readFileSync(`${__dirname}/challenges/4-svg animation.html`, 'utf-8');
   const error = () => {
     res.writeHead(404, {
-    'Content-type': 'text/html',
-    'challenge-header': '404 Not found'
-  });
-  res.end('opps! page not found(404).');
+      'Content-type': 'text/html',
+      'challenge-header': '404 Not found'
+    });
+    res.end('opps! page not found(404).');
   }
 
-  if(path == '/' || path == '/home'){ 
+  if (path == '/' || path == '/home') {
     res.writeHead(200, {
       'Content-type': 'text/html',
       'project-header': 'first-node-project'
@@ -30,20 +30,20 @@ const server = http.createServer((req,res) => {
     res.end(home);
   }
 
-  else if(path.includes('/challenge')){
+  else if (path.includes('/challenge')) {
     res.writeHead(200, {
       'Content-type': 'text/html',
       'challenge-header': `${path.slice(1)}`
     });
-    if(path === '/challenge-1') res.end(challenge1);
-    else if(path === '/challenge-2') res.end(challenge2);
-    else if(path === '/challenge-3') res.end(challenge3);
-    else if(path === '/challenge-4') res.end(challenge4);
+    if (path === '/challenge-1') res.end(challenge1);
+    else if (path === '/challenge-2') res.end(challenge2);
+    else if (path === '/challenge-3') res.end(challenge3);
+    else if (path === '/challenge-4') res.end(challenge4);
     else error();
   }
   else error();
 });
 
 server.listen(port, hostname, () => {
-  console.log(`server Running at ${port} `)
+  console.log(`server Running at ${port} from ${__dirname}`)
 })
